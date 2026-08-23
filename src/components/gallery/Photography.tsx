dc691e3335be5b2e5242b7626cff1375e3709129
CmdLari <studio-lp@gmx.de>
2026-08-23 20:42:19 +0200

    complete rehaul

---
+++ src/components/gallery/Photography.tsx
@@ -0,0 +1,28 @@
+import Gallery from "../Gallery";
+
+const screenshots = [
+    "/src/assets/images/gallery/design/Design01.jpg",
+    "/src/assets/images/gallery/design/Design02.jpg",
+    "/src/assets/images/gallery/design/Design03.jpg",
+    "/src/assets/images/gallery/design/Design04.jpg",
+    "/src/assets/images/gallery/design/Design05.jpg",
+    "/src/assets/images/gallery/design/Design06.jpg",
+    "/src/assets/images/gallery/design/Design07.jpg",
+    "/src/assets/images/gallery/design/Design08.jpg",
+    "/src/assets/images/gallery/design/Design09.jpg",
+    "/src/assets/images/gallery/design/Design10.jpg",
+    "/src/assets/images/gallery/design/Design11.jpg",
+    "/src/assets/images/gallery/design/Design12.jpg",
+];            
+
+function Photography() {
+    return (
+        <main className="site-layout__content">
+            <p>
+                <h2>under construction</h2>
+            </p>
+        </main>
+    );
+}
+
+export default Photography;
